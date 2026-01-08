0d5e5feee595b7d6a1f10ac9cf8e7b63c5147ebe
taehajs <taehajs@naver.com>
2026-01-08 17:21:37 +0900

    asdfasdf

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
-/* ******************************************
+/******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
- *******************************************/
-/* ***********************
+ ******************************************/
+
+/***********************
  * Require Statements
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+require("dotenv").config()
+const path = require("path")
 
-/* ***********************
- * Routes
+const app = express()
+
+/***********************
+ * View Engine and Layouts
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout ") // not at views root
+app.set("layout", "./layouts/layout") 
+app.set("views", path.join(__dirname, "views"))
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
+/***********************
+ * Static Files
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+app.use(express.static(path.join(__dirname, "public")))
 
-/* ***********************
- * Log statement to confirm server operation
+/***********************
+ * Index Route
+ *************************/
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
+
+/***********************
+ * Local Server Information
+ *************************/
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
+
+/***********************
+ * Server Start
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
-
-//Index Route
-app.get("/", function(req, res) {
-  res.render("index", { title: "Home" });
-});
