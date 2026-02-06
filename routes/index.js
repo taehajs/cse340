bf9d5e3d6c25aa40d312766af806bd25b382d102
taehajs <taehajs@naver.com>
2026-02-06 21:36:13 +0900

    v h

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const classificationModel = require("../models/classification-model")
 
 
 
-router.get("/", async (req, res) => {
-  const classifications = await classificationModel.getClassifications()
-  const nav = utilities.buildNav(classifications)
-  res.render("index", { title: "Home", nav })
+router.get("/", async (req, res, next) => {
+  try {
+    const classifications = await classificationModel.getClassifications()
+    const nav = utilities.buildNav(classifications)
+    res.render("index", { title: "Home", nav })
+  } catch (error) {
+    next(error)
+  }
 })
-
-
 
 module.exports = router
