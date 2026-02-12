4346206e7a3f390aaaa1d68e9742576d48c22f3a
taehajs <taehajs@naver.com>
2026-02-12 19:22:54 +0900

    asdfg

--- utilities/index.js
+++ utilities/index.js
@@ -1,26 +1,54 @@
+const invModel = require("../models/inventory-model")
+
+async function buildNav() {
+  const data = await invModel.getClassifications()
+
+  let list = "<ul>"
+  list += '<li><a href="/" title="Home page">Home</a></li>'
+
+  data.rows.forEach((row) => {
+    list += `
+      <li>
+        <a href="/inv/type/${row.classification_id}" 
+        title="See our inventory of ${row.classification_name} vehicles">
+        ${row.classification_name}
+        </a>
+      </li>
+    `
+  })
+
+  list += "</ul>"
+  return list
+}
+
+
+
 function buildVehicleDetail(vehicle) {
-  const price = vehicle.price.toLocaleString("en-US", {
+  const price = vehicle.inv_price.toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
-  });
-  const mileage = vehicle.miles.toLocaleString("en-US");
+  })
+
+  const mileage = vehicle.inv_miles.toLocaleString("en-US")
 
   return `
     <section class="vehicle-detail">
       <div class="vehicle-image">
-        <img src="${vehicle.image}" alt="Image of ${vehicle.make} ${vehicle.model}">
+        <img src="${vehicle.inv_image}" 
+             alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
       </div>
-
 
       <div class="vehicle-info">
-        <h1>${vehicle.year} ${vehicle.make} ${vehicle.model}</h1>
+        <h1>${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}</h1>
         <p><strong>Price:</strong> ${price}</p>
         <p><strong>Mileage:</strong> ${mileage} miles</p>
-        <p><strong>Description:</strong> ${vehicle.description}</p>
+        <p><strong>Description:</strong> ${vehicle.inv_description}</p>
       </div>
     </section>
-  `;
+  `
 }
 
-
-module.exports = { buildVehicleDetail };
+module.exports = {
+  buildNav,
+  buildVehicleDetail
+}
