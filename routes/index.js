fe24fd70defdc2efe1972c49eeb44d32fecb2291
taehajs <taehajs@naver.com>
2026-02-12 23:25:33 +0900

    fasg

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
 const express = require("express")
 const router = express.Router()
-const utilities = require("../utilities")
+const invModel = require("../models/inventory-model")
 const classificationModel = require("../models/classification-model")
-
+const utilities = require("../utilities")
 
 
 router.get("/", async (req, res, next) => {
   try {
     const classifications = await classificationModel.getClassifications()
+    const inventory = await invModel.getAllInventory() 
     const nav = utilities.buildNav(classifications)
-    res.render("index", { title: "Home", nav })
-  } catch (error) {
-    next(error)
+    res.render("home", {
+      title: "Home",
+      nav,
+      classifications,
+      inventory,
+      message: req.session.message || null
+    })
+  } catch (err) {
+    next(err)
   }
 })
 
