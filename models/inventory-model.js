b4d76bfcbd2eb4032c1ff22aca50238aa6a5bc38
taehajs <taehajs@naver.com>
2026-02-12 18:31:38 +0900

    plz..

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,14 +1,13 @@
-const pool = require("../database");
-
+const pool = require("../database")
 async function insertClassification(name) {
   try {
     const sql =
-      "INSERT INTO classification (classification_name) VALUES ($1)";
-    const data = await pool.query(sql, [name]);
-    return data.rowCount > 0;
+      "INSERT INTO classification (classification_name) VALUES ($1)"
+    const data = await pool.query(sql, [name])
+    return data.rowCount > 0
   } catch (error) {
-    console.error("insertClassification error:", error);
-    return false;
+    console.error("insertClassification error:", error)
+    return false
   }
 }
 
@@ -22,20 +21,20 @@ async function insertVehicle(
   try {
     const sql = `
       INSERT INTO inventory
-      (make, model, year, price, classification_id)
+      (inv_make, inv_model, inv_year, inv_price, classification_id)
       VALUES ($1, $2, $3, $4, $5)
-    `;
+    `
     const data = await pool.query(sql, [
       make,
       model,
       parseInt(year),
       parseFloat(price),
       classification_id,
-    ]);
-    return data.rowCount > 0;
+    ])
+    return data.rowCount > 0
   } catch (error) {
-    console.error("insertVehicle error:", error);
-    return false;
+    console.error("insertVehicle error:", error)
+    return false
   }
 }
 
@@ -45,15 +44,14 @@ async function getInventoryByClassificationId(classification_id) {
       SELECT *
       FROM inventory
       WHERE classification_id = $1
-    `;
-    const data = await pool.query(sql, [classification_id]);
-    return data.rows;
+    `
+    const data = await pool.query(sql, [classification_id])
+    return data.rows
   } catch (error) {
-    console.error("getInventoryByClassificationId error:", error);
-    throw error;
+    console.error("getInventoryByClassificationId error:", error)
+    throw error
   }
 }
-
 
 async function getVehicleById(invId) {
   try {
@@ -61,19 +59,18 @@ async function getVehicleById(invId) {
       SELECT *
       FROM inventory
       WHERE inv_id = $1
-    `;
-    const data = await pool.query(sql, [invId]);
-    return data.rows[0];
+    `
+    const data = await pool.query(sql, [invId])
+    return data.rows[0]
   } catch (error) {
-    console.error("getVehicleById error:", error);
-    throw error;
+    console.error("getVehicleById error:", error)
+    throw error
   }
 }
-
 
 module.exports = {
   insertClassification,
   insertVehicle,
   getInventoryByClassificationId,
   getVehicleById,
-};
+}
