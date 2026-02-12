13f28d55cda8b2db822ad72f8be0c76e60c6508a
taehajs <taehajs@naver.com>
2026-02-12 23:43:47 +0900

    sadg

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,25 @@
 const express = require("express")
 const router = express.Router()
-
 const invModel = require("../models/inventory-model")
-
 const classificationModel = require("../models/classification-model")
-
 const utilities = require("../utilities")
 
-
-const { requireAuth } = require("../middleware/authMiddleware")
 
 router.get("/", async (req, res, next) => {
   try {
     const classifications = await classificationModel.getClassifications()
     const inventory = await invModel.getAllInventory()
     const nav = utilities.buildNav(classifications)
+
+
+    const user = req.user || null
+
     res.render("home", {
       title: "Home",
       nav,
       classifications,
       inventory,
-      user: req.user || null,
-
-
+      user,
       message: req.session.message || null
     })
   } catch (err) {
