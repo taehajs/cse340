29c992522e7d899d6aa70349ab89e700e1c6a535
taehajs <taehajs@naver.com>
2026-02-12 23:18:12 +0900

    gggg

--- utilities/index.js
+++ utilities/index.js
@@ -3,7 +3,7 @@ const classificationModel = require("../models/classification-model");
 async function buildNav() {
   const data = await classificationModel.getClassifications();
   let list = "<ul>";
-  
+
   list += '<li><a href="/" title="Home page">Home</a></li>';
   data.forEach((row) => {
     list += `<li><a href="/inventory/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">${row.classification_name}</a></li>`;
@@ -15,13 +15,12 @@ async function buildNav() {
 function buildVehicleDetail(vehicle) {
   const price = vehicle.inv_price.toLocaleString("en-US", { style: "currency", currency: "USD" });
   const mileage = vehicle.inv_miles.toLocaleString("en-US");
-  return `
 
+  return `
     <section class="vehicle-detail">
       <div class="vehicle-image">
         <img src="${vehicle.inv_image}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
       </div>
-
       <div class="vehicle-info">
         <h1>${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}</h1>
         <p><strong>Price:</strong> ${price}</p>
@@ -30,13 +29,19 @@ function buildVehicleDetail(vehicle) {
       </div>
     </section>
   `;
-
 }
 
-async function buildClassificationGrid(data) {
+
+async function buildClassificationGrid(vehicleData) {
+  if (!vehicleData || vehicleData.length === 0) return "<p>No vehicles found.</p>";
+
   let grid = '<ul class="classification-grid">';
-  data.forEach((row) => {
-    grid += `<li><a href="/inventory/detail/${row.inv_id}" title="${row.inv_make} ${row.inv_model}">${row.inv_year} ${row.inv_make} ${row.inv_model}</a></li>`;
+  vehicleData.forEach((vehicle) => {
+    grid += `<li>
+      <a href="/inventory/detail/${vehicle.inv_id}" title="View details of ${vehicle.inv_make} ${vehicle.inv_model}">
+        ${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}
+      </a>
+    </li>`;
   });
   grid += "</ul>";
   return grid;
