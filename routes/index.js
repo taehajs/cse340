e2cd8d5dbbc1ede8a556b6f15fb9f3f1208e003a
taehajs <taehajs@naver.com>
2026-02-12 23:53:25 +0900

    GDASD

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,35 @@ router.get("/", async (req, res, next) => {
     const inventory = await invModel.getAllInventory()
     const nav = utilities.buildNav(classifications)
 
-
-    const user = req.user || null
-
     res.render("home", {
       title: "Home",
       nav,
       classifications,
       inventory,
-      user,
+      user: req.user || null,
       message: req.session.message || null
     })
+  } catch (err) {
+    next(err)
+  }
+})
+
+
+router.get("/classification/:id", async (req, res, next) => {
+  try {
+    const classifications = await classificationModel.getClassifications()
+    const nav = utilities.buildNav(classifications)
+    const classificationId = req.params.id
+    const inventory = await invModel.getInventoryByClassificationId(classificationId)
+    const classification = classifications.find(c => c.classification_id == classificationId)
+
+    res.render("inventory/classification", {
+      title: classification ? classification.classification_name : "Classification",
+      nav,
+      inventory,
+      user: req.user || null
+    })
+    
   } catch (err) {
     next(err)
   }
