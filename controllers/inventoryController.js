9c7d38f10354bc920e9c4c4a4255cc421b99b257
taehajs <taehajs@naver.com>
2026-02-18 15:52:22 +0900

    w3

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -4,15 +4,22 @@ const utilities = require("../utilities");
 async function getVehicleDetail(req, res, next) {
   try {
     const invId = parseInt(req.params.invId);
+
     const vehicle = await invModel.getVehicleById(invId);
+
     if (!vehicle) {
       return next({ status: 404, message: "Vehicle not found" });
     }
-    const html = utilities.buildVehicleDetailHTML(vehicle);
+
+    const nav = await utilities.buildNav();
+    const html = utilities.buildVehicleDetail(vehicle);
+
     res.render("inventory/detail", {
       title: `${vehicle.inv_make} ${vehicle.inv_model}`,
+      nav,
       content: html
     });
+
   } catch (error) {
     next(error);
   }
--- utilities/index.js
+++ utilities/index.js
@@ -5,22 +5,35 @@ async function buildNav() {
   let list = "<ul>";
 
   list += '<li><a href="/" title="Home page">Home</a></li>';
+
   data.forEach((row) => {
-    list += `<li><a href="/inventory/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">${row.classification_name}</a></li>`;
+    list += `<li>
+      <a href="/inv/type/${row.classification_id}" 
+         title="See our inventory of ${row.classification_name} vehicles">
+         ${row.classification_name}
+      </a>
+    </li>`;
   });
+
   list += "</ul>";
   return list;
 }
 
 function buildVehicleDetail(vehicle) {
-  const price = vehicle.inv_price.toLocaleString("en-US", { style: "currency", currency: "USD" });
+  const price = vehicle.inv_price.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+
   const mileage = vehicle.inv_miles.toLocaleString("en-US");
 
   return `
     <section class="vehicle-detail">
       <div class="vehicle-image">
-        <img src="${vehicle.inv_image}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
+        <img src="${vehicle.inv_image}" 
+             alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
       </div>
+
       <div class="vehicle-info">
         <h1>${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}</h1>
         <p><strong>Price:</strong> ${price}</p>
@@ -31,20 +44,27 @@ function buildVehicleDetail(vehicle) {
   `;
 }
 
-
 async function buildClassificationGrid(vehicleData) {
-  if (!vehicleData || vehicleData.length === 0) return "<p>No vehicles found.</p>";
+  if (!vehicleData || vehicleData.length === 0)
+    return "<p>No vehicles found.</p>";
 
   let grid = '<ul class="classification-grid">';
+
   vehicleData.forEach((vehicle) => {
     grid += `<li>
-      <a href="/inventory/detail/${vehicle.inv_id}" title="View details of ${vehicle.inv_make} ${vehicle.inv_model}">
-        ${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}
+      <a href="/inv/detail/${vehicle.inv_id}" 
+         title="View details of ${vehicle.inv_make} ${vehicle.inv_model}">
+         ${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}
       </a>
     </li>`;
   });
+
   grid += "</ul>";
   return grid;
 }
 
-module.exports = { buildNav, buildVehicleDetail, buildClassificationGrid };
+module.exports = {
+  buildNav,
+  buildVehicleDetail,
+  buildClassificationGrid
+};
