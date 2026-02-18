fbfd334ea511b3cd7267216c2e24b79539b687dc
taehajs <taehajs@naver.com>
2026-02-18 18:21:16 +0900

    gasdashg

--- controllers/classificationController.js
+++ controllers/classificationController.js
@@ -4,7 +4,7 @@ const utilities = require("../utilities")
 exports.buildClassificationForm = (req, res, next) => {
   res.render("inventory/add-classification", {
     title: "Add Classification",
-    nav: utilities.getNav(),
+    nav: utilities.buildNav([]),
     message: req.flash("message"),
   })
 }
@@ -14,17 +14,17 @@ exports.addClassification = async (req, res, next) => {
     const { classification_name } = req.body
     if (!classification_name) {
       req.flash("message", "Please provide a classification name")
-      return res.redirect("/inventory/add-classification")
+      return res.redirect("/inv/add-classification")
     }
 
     const result = await classificationModel.insertClassification(classification_name)
 
     if (result) {
       req.flash("message", `Classification '${classification_name}' added successfully!`)
-      res.redirect("/inventory/management")
+      res.redirect("/inv/management")
     } else {
       req.flash("message", "Failed to add classification")
-      res.redirect("/inventory/add-classification")
+      res.redirect("/inv/add-classification")
     }
   } catch (error) {
     next(error)
--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -2,13 +2,12 @@ const inventoryModel = require("../models/inventory-model")
 const classificationModel = require("../models/classification-model")
 const utilities = require("../utilities")
 
-
 exports.buildAddVehicleForm = async (req, res, next) => {
   try {
     const classifications = await classificationModel.getClassifications()
     res.render("inventory/add-vehicle", {
       title: "Add Vehicle",
-      nav: utilities.getNav(),
+      nav: utilities.buildNav(classifications),
       classifications,
       message: req.flash("message"),
     })
@@ -23,17 +22,17 @@ exports.addVehicle = async (req, res, next) => {
 
     if (!inv_make || !inv_model || !inv_year || !inv_price || !classification_id) {
       req.flash("message", "Please provide all fields")
-      return res.redirect("/inventory/add-vehicle")
+      return res.redirect("/inv/add-vehicle")
     }
 
     const result = await inventoryModel.insertVehicle(inv_make, inv_model, inv_year, inv_price, classification_id)
 
     if (result) {
       req.flash("message", `${inv_make} ${inv_model} added successfully!`)
-      res.redirect("/inventory/management")
+      res.redirect("/inv/management")
     } else {
       req.flash("message", "Failed to add vehicle")
-      res.redirect("/inventory/add-vehicle")
+      res.redirect("/inv/add-vehicle")
     }
   } catch (error) {
     next(error)
