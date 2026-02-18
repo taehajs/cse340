4edfef3577a99fb3b02197711d86b952abbd60e1
taehajs <taehajs@naver.com>
2026-02-18 18:41:24 +0900

    ahafasdgasdfasgasdgas

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,10 +3,8 @@ const router = express.Router()
 const inventoryController = require("../controllers/inventoryController")
 const classificationController = require("../controllers/classificationController")
 
-
 router.get("/add-classification", classificationController.buildClassificationForm)
 router.post("/add-classification", classificationController.addClassification)
-
 
 router.get("/add-vehicle", inventoryController.buildAddVehicleForm)
 router.post("/add-vehicle", inventoryController.addVehicle)
